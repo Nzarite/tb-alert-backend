601018388ebdecb3a34a6edfab13d1871b8943ff
Akshit Chauhan <akshitsingh.chauhan@beehyv.com>
2025-01-30 17:00:32 +0530

    initial commit

---
+++ src/main/java/com/beehyv/tbalert/tbalertbackend/TbAlertBackendApplication.java
@@ -0,0 +1,13 @@
+package com.beehyv.tbalert.tbalertbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbAlertBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TbAlertBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beehyv/tbalert/tbalertbackend/TbAlertBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beehyv.tbalert.tbalertbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbAlertBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
