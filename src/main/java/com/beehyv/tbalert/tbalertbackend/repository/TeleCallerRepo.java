d554c54e8569a32c09bf61f2855794be4013068b
Tanishq Chaurasia <tanishq.chaurasia@beehyv.com>
2025-02-28 11:33:01 +0530

    fixed telecaller and statehead search showing deleted entries also

--- src/main/java/com/beehyv/tbalert/tbalertbackend/repository/TeleCallerRepo.java
+++ src/main/java/com/beehyv/tbalert/tbalertbackend/repository/TeleCallerRepo.java
@@ -3,6 +3,8 @@ package com.beehyv.tbalert.tbalertbackend.repository;
 import com.beehyv.tbalert.tbalertbackend.dto.output.TeleCallerOutputDTO;
 import com.beehyv.tbalert.tbalertbackend.entity.TeleCaller;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
@@ -22,4 +24,7 @@ public interface TeleCallerRepo extends JpaRepository<TeleCaller, Long> {
     List<TeleCaller> findAllByPerson_IsDeletedTrue();
 
     List<TeleCaller> findAllByPerson_IsDeletedTrueAndPerson_Address_State_StateName(String state);
+
+    @Query("SELECT t FROM TeleCaller t WHERE t.person.isDeleted = FALSE AND (t.person.firstName LIKE %:name% OR t.person.lastName LIKE %:name%)")
+    List<TeleCaller> findAllByPerson_IsDeletedFalseAndPerson_FirstNameContainingIgnoreCaseOrPerson_LastNameContainingIgnoreCase(@Param("name") String name);
 }
--- src/main/java/com/beehyv/tbalert/tbalertbackend/service/impl/StateHeadServiceImpl.java
+++ src/main/java/com/beehyv/tbalert/tbalertbackend/service/impl/StateHeadServiceImpl.java
@@ -116,7 +116,7 @@ public class StateHeadServiceImpl implements StateHeadService {
 
     @Override
     public List<StateHeadOutputDTO> getStateHeadByName(String name) {
-        List<StateHead> stateHeads = stateHeadRepo.findAllByPerson_FirstNameContainingIgnoreCaseOrPerson_LastNameContainingIgnoreCase(name, name);
+        List<StateHead> stateHeads = stateHeadRepo.findAllByPerson_FirstNameContainingIgnoreCaseOrPerson_LastNameContainingIgnoreCase(name);
         return stateHeads.stream().map(stateHeadMapper::toStateHeadOutputDTO).toList();
     }
 
