00a54daf09f3375ec5f972b34bf6446199c2007e
Akshit Chauhan <akshitsingh.chauhan@beehyv.com>
2025-03-03 16:45:36 +0530

    sms reminder time added refactor

--- src/main/java/com/beehyv/tbalert/tbalertbackend/dto/input/PatientInputDTO.java
+++ src/main/java/com/beehyv/tbalert/tbalertbackend/dto/input/PatientInputDTO.java
@@ -49,7 +49,6 @@ public class PatientInputDTO {
     @NotNull(message = "Provide the patient consent for message reminders")
     private Boolean consentForMessage;
 
-    @NotNull(message = "Provide the sms time")
     private String reminderTime;
 
 }
